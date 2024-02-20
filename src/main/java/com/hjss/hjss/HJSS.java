2e61ce8ef49c706fc3e285591c345f13145ca0a6
mandar145 <cmandar41@gmail.com>
2024-02-20 20:44:29 +0000

    first commit

---
+++ src/main/java/com/hjss/hjss/HJSS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hjss.hjss;
+
+/**
+ *
+ * @author Madar
+ */
+public class HJSS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
