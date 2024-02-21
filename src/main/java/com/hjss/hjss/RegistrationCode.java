27bd056e4f8a6082139f02dc2e69957cbf0b58e6
mandar145 <cmandar41@gmail.com>
2024-02-21 02:44:02 +0000

    Continuation - added: Registration Class and Registration Jform, Function for predefined values of  level added.

--- src/main/java/com/hjss/hjss/RegistrationCode.java
+++ src/main/java/com/hjss/hjss/RegistrationCode.java
@@ -12,14 +12,37 @@ public class RegistrationCode {
     
     private String name; 
     private int age;
-    private String ID;
-    private String level;
+    private int level;
+    private int ID;
     
     //constructor
-    public RegistrationCode(String name,int age,String ID, String level){
+    public RegistrationCode(String name,int age, int level, int ID){
         this.name = name;
         this.age=age;
-        
-        
+        this.level=level;
+        this.ID=ID;
             }
+    
+    /**
+     *
+     * @return
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+    public int getLevel()
+    {
+        return level;
+    }   
+    public int getID()
+    {
+        return ID;
+    }       
+    
 }
